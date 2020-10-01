b9b9c881852800593853035c3b4ff278312f99e1
nachiket-personal <nachiketbhosale21@gmail.com>
2020-10-01 11:28:05 +0530

    Add SparkHandler code

--- src/main/scala/com/test/gitDemo/HelloScala.scala
+++ src/main/scala/com/test/gitDemo/HelloScala.scala
@@ -4,7 +4,17 @@ object HelloScala {
   def main(args: Array[String]): Unit = {
     println("Hello Scala Git Demo 1234")
     println("Hello I am Nachiket")
-    println("Hello I am Nachiket")
+
+    println("Read Argument :- "+args.toString)
+    val max = args.length
+
+    for(i <- 0 until max by 2){
+      var key = args(i)
+      var value = args(i + 1)
+      println("key :- "+key)
+      println("value :- "+value)
+    }
+    println("Print Argument :- "+max)
   }
 
 }
